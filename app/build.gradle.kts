1b870b56681520764d5f91cdb770f6e92558df23
Yahya <yahyatinani@gmail.com>
2021-07-24 16:56:00 +0100

    Add recompose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,9 +55,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation(project(mapOf("path" to ":recompose")))
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 }
