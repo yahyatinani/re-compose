b7fe6ef588dd91a74bd640813a5591af72c202e4
Yahya <yahyatinani@gmail.com>
2021-09-14 13:12:23 +0100

    Clear the reactions' cache from inactive reactions and dispose them

--- recompose/src/main/java/com/github/whyrising/recompose/subs/subs.kt
+++ recompose/src/main/java/com/github/whyrising/recompose/subs/subs.kt
@@ -1,10 +1,6 @@
 package com.github.whyrising.recompose.subs
 
 import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.github.whyrising.recompose.db.appDb
 import com.github.whyrising.recompose.registrar.Kinds
 import com.github.whyrising.recompose.registrar.Kinds.Sub
@@ -13,20 +9,13 @@ import com.github.whyrising.recompose.registrar.registerHandler
 import com.github.whyrising.y.collections.core.get
 import com.github.whyrising.y.collections.core.m
 import com.github.whyrising.y.collections.core.v
+import com.github.whyrising.y.collections.vector.IPersistentVector
 import com.github.whyrising.y.concurrency.Atom
-import com.github.whyrising.y.concurrency.IAtom
-import com.github.whyrising.y.concurrency.IDeref
 import com.github.whyrising.y.concurrency.atom
-import com.github.whyrising.y.core.str
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.updateAndGet
-import kotlinx.coroutines.launch
 import java.lang.ref.SoftReference
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.reflect.KProperty
 
 val kind: Kinds = Sub
@@ -59,10 +48,33 @@ val queryReaction = atom(m<Any, Any>())
 // -- subscribe -----------------------------------------------------------------
 internal const val TAG = "re-compose"
 
+private fun <T> cacheReaction(
+    cacheKey: IPersistentVector<Any>,
+    reaction: Reaction<T>
+): Reaction<T> {
+    reaction.addOnDispose { r: Reaction<T> ->
+        queryReaction.swap { cache ->
+            if (cache.containsKey(cacheKey) && r === get(cache, cacheKey)) {
+                Log.i(
+                    "cacheReaction",
+                    "${get(cacheKey, 0)} got removed from cache."
+                )
+                cache.dissoc(cacheKey)
+            } else cache
+        }
+    }
+
+    queryReaction.swap { qCache ->
+        qCache.assoc(cacheKey, reaction)
+    }
+
+    return reaction
+}
+
 internal fun <T> subscribe(qvec: List<Any>): Reaction<T> {
     val queryId = qvec[0]
     val handlerFn = getHandler(kind, queryId)
-        as ((db: Atom<*>, qvec: List<Any>) -> Reaction<Any>)?
+        as ((db: Atom<*>, qvec: List<Any>) -> Reaction<T>)?
 
     if (handlerFn == null) {
         Log.e(TAG, "no subscription handler registered for id: `$queryId`")
@@ -71,7 +83,6 @@ internal fun <T> subscribe(qvec: List<Any>): Reaction<T> {
         )
     }
     val cacheKey = v(qvec, v<Any>())
-//    val cache = subsCache[cacheKey]
     val cachedReaction = get(queryReaction(), cacheKey)
 
     if (cachedReaction != null) {
@@ -80,21 +91,9 @@ internal fun <T> subscribe(qvec: List<Any>): Reaction<T> {
     }
 
     Log.i(TAG, "No cache was found for subscription `$cacheKey`")
-
     val reaction = handlerFn(appDb, qvec)
 
-    queryReaction.swap { qCache ->
-        qCache.assoc(cacheKey, reaction)
-    }
-
-//    if (subsCache.contains(cacheKey) && r == subsCache[cacheKey]) {
-//        Log.i(TAG, "replace cache `$cacheKey`")
-//        subsCache.remove(cacheKey)
-//    }
-
-//    subsCache[cacheKey] = r
-
-    return reaction as Reaction<T>
+    return cacheReaction(cacheKey, reaction)
 }
 
 // -- regSub -----------------------------------------------------------------
@@ -138,82 +137,3 @@ internal fun <T, R> regSub(
 
     registerHandler(queryId, kind, subsHandlerFn)
 }
-
-class Reaction<T>(val f: () -> T) : ViewModel(), IDeref<T>, IAtom<T> {
-    internal val state: MutableStateFlow<T> by lazy { MutableStateFlow(f()) }
-
-    val id: String by lazy { str("rx", hashCode()) }
-
-    override fun deref(): T = state.value
-
-    override fun reset(newValue: T): T = state.updateAndGet { newValue }
-
-    /**
-     * This function use a regular comparison using [Any.equals].
-     * If [f] returns a value that is equal to the current stored value in the
-     * reaction, this function does not actually change the reference that is
-     * stored in the [state].
-     *
-     * [f] may be evaluated multiple times, if [state] is being concurrently
-     * updated.
-     *
-     * This method is **thread-safe** and can be safely invoked from concurrent
-     * coroutines without external synchronization.
-     */
-    override fun swap(f: (currentVal: T) -> T): T = state.updateAndGet(f)
-
-    override fun <A> swap(arg: A, f: (currentVal: T, arg: A) -> T): T {
-        while (true) {
-            val currentVal = state.value
-            val newVal = f(currentVal, arg)
-
-            if (state.compareAndSet(currentVal, newVal))
-                return newVal
-        }
-    }
-
-    override fun <A1, A2> swap(
-        arg1: A1,
-        arg2: A2,
-        f: (currentVal: T, arg1: A1, arg2: A2) -> T
-    ): T {
-        while (true) {
-            val currentVal = state.value
-            val newVal = f(currentVal, arg1, arg2)
-
-            if (state.compareAndSet(currentVal, newVal))
-                return newVal
-        }
-    }
-}
-
-/**
- * This function runs the [computation] function every time the [inputNode]
- * changes.
- *
- * @param inputNode reaction which extract data directly from [appDb],
- * but do no further computation.
- * @param context for the coroutines running under [viewModelScope].
- * @param computation a function that obtains data from [inputNode], and compute
- * derived data from it.
- */
-internal inline fun <T, R> Reaction<R>.reactTo(
-    inputNode: Reaction<T>,
-    context: CoroutineContext,
-    crossinline computation: suspend (newInput: T) -> R
-) {
-    viewModelScope.launch(context) {
-        inputNode.state.collect { newInput: T ->
-            // Evaluate this only once by leaving it out of swap,
-            // since swap can run f multiple times, the output is the same for
-            // the same input
-            val materializedView = computation(newInput)
-            swap { materializedView }
-        }
-    }
-}
-
-@Composable
-fun <T> Reaction<T>.watch(
-    context: CoroutineContext = EmptyCoroutineContext
-): T = state.collectAsState(context = context).value
