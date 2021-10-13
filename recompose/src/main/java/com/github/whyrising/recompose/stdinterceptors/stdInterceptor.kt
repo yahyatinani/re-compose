cab9a8f11ad2f5f8987e4cd0ecc435551e185450
Yahya <yahyatinani@gmail.com>
2021-10-13 12:33:01 +0100

    Test fxHandlerToInterceptor(handler)

--- recompose/src/main/java/com/github/whyrising/recompose/stdinterceptors/stdInterceptor.kt
+++ recompose/src/main/java/com/github/whyrising/recompose/stdinterceptors/stdInterceptor.kt
@@ -4,7 +4,6 @@ import com.github.whyrising.recompose.cofx.Coeffects
 import com.github.whyrising.recompose.events.Event
 import com.github.whyrising.recompose.fx.Effects
 import com.github.whyrising.recompose.interceptor.Context
-import com.github.whyrising.recompose.interceptor.Interceptor
 import com.github.whyrising.recompose.interceptor.toInterceptor
 import com.github.whyrising.recompose.schemas.ContextSchema.coeffects
 import com.github.whyrising.recompose.schemas.ContextSchema.effects
@@ -20,27 +19,29 @@ import com.github.whyrising.y.collections.core.l
 These 2 factories wrap 2 kinds of event handlers.
 */
 
+typealias DbEventHandler<T> = (db: T, event: Event) -> T
+typealias FxEventHandler = (cofx: Coeffects, event: Event) -> Effects
+
 inline fun <T : Any> dbHandlerToInterceptor(
-    crossinline dbEventHandler: (db: T, event: Event) -> T
-): Interceptor = toInterceptor(
+    crossinline handler: DbEventHandler<T>
+) = toInterceptor(
     id = ":db-handler",
     before = { context: Context ->
         val cofx = context[coeffects] as Coeffects
         assocIn(
             context,
             l(effects, db),
-            dbEventHandler(cofx[db] as T, cofx[event] as Event)
+            handler(cofx[db] as T, cofx[event] as Event)
         ) as Context
     }
 )
 
-inline fun fxHandlerToInterceptor(
-    crossinline fxEventHandler: (cofx: Coeffects, event: Event) -> Effects
-): Interceptor = toInterceptor(
-    id = ":fx-handler",
-    before = { context ->
-        val cofx = context[coeffects] as Coeffects
-        val event = cofx[event] as Event
-        context.assoc(effects, fxEventHandler(cofx, event))
-    }
-)
+inline fun fxHandlerToInterceptor(crossinline handler: FxEventHandler) =
+    toInterceptor(
+        id = ":fx-handler",
+        before = { context ->
+            val cofx = context[coeffects] as Coeffects
+            val event = cofx[event] as Event
+            context.assoc(effects, handler(cofx, event))
+        }
+    )
--- recompose/src/test/java/com/github/whyrising/recompose/StdInterceptorTest.kt
+++ recompose/src/test/java/com/github/whyrising/recompose/StdInterceptorTest.kt
@@ -1,13 +1,15 @@
 package com.github.whyrising.recompose
 
 import com.github.whyrising.recompose.cofx.Coeffects
-import com.github.whyrising.recompose.events.Event
 import com.github.whyrising.recompose.interceptor.Context
 import com.github.whyrising.recompose.interceptor.Interceptor
 import com.github.whyrising.recompose.interceptor.InterceptorFn
 import com.github.whyrising.recompose.schemas.ContextSchema
 import com.github.whyrising.recompose.schemas.Schema
+import com.github.whyrising.recompose.stdinterceptors.DbEventHandler
+import com.github.whyrising.recompose.stdinterceptors.FxEventHandler
 import com.github.whyrising.recompose.stdinterceptors.dbHandlerToInterceptor
+import com.github.whyrising.recompose.stdinterceptors.fxHandlerToInterceptor
 import com.github.whyrising.y.collections.core.get
 import com.github.whyrising.y.collections.core.m
 import com.github.whyrising.y.collections.core.v
@@ -18,7 +20,7 @@ class StdInterceptorTest : FreeSpec({
     "dbHandlerToInterceptor() should add new db value to effects in Context" {
         val coeffects: Coeffects = m(Schema.db to 1, Schema.event to v("id", 5))
         val context: Context = m(ContextSchema.coeffects to coeffects)
-        val addToDbHandler = { db: Int, event: Event ->
+        val addToDbHandler: DbEventHandler<Int> = { db, event ->
             db + event[1] as Int
         }
 
@@ -29,6 +31,22 @@ class StdInterceptorTest : FreeSpec({
         newContext shouldBe m(
             ContextSchema.coeffects to coeffects,
             ContextSchema.effects to m(Schema.db to 6)
+        )
+    }
+
+    "fxHandlerToInterceptor() should assoc new effects value to Context" {
+        val coeffects: Coeffects = m(Schema.db to 1, Schema.event to v("id", 5))
+        val context: Context = m(ContextSchema.coeffects to coeffects)
+        val newEffects = m("fx-test" to 15)
+        val handler: FxEventHandler = { _, _ -> newEffects }
+
+        val interceptor: Interceptor = fxHandlerToInterceptor(handler)
+        val fn: InterceptorFn = interceptor[Schema.before] as InterceptorFn
+        val newContext = fn(context)
+
+        newContext shouldBe m(
+            ContextSchema.coeffects to coeffects,
+            ContextSchema.effects to newEffects
         )
     }
 })
