3f4a2364ce4651d3d152f6ffc94c43ca7d69a7cf
Yahya <47183730+whyrising@users.noreply.github.com>
2022-02-14 09:34:15 +0000

    Implement digital watch UI in :example

--- example/src/main/java/com/github/whyrising/recompose/example/MainActivity.kt
+++ example/src/main/java/com/github/whyrising/recompose/example/MainActivity.kt
@@ -1,43 +1,76 @@
 package com.github.whyrising.recompose.example
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.whyrising.recompose.example.home.DigitalWatch
 import com.github.whyrising.recompose.example.ui.theme.RecomposeTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            RecomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+@Composable
+fun MyApp() {
+    RecomposeTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar {
+                    Text(
+                        text = "Re-compose Sample",
+                        style = MaterialTheme.typography.h5,
+                    )
+                }
+            },
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+                    DigitalWatch()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+// -- App Preview --------------------------------------------------------------
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RecomposeTheme {
-        Greeting("Android")
+        MyApp()
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultDarkPreview() {
+    RecomposeTheme {
+        MyApp()
+    }
+}
+
+// -- Entry --------------------------------------------------------------------
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
     }
 }
---
+++ example/src/main/java/com/github/whyrising/recompose/example/home/digital_watch.kt
@@ -0,0 +1,63 @@
+package com.github.whyrising.recompose.example.home
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.whyrising.recompose.example.ui.theme.RecomposeTheme
+
+@Composable
+fun DigitalWatch(modifier: Modifier = Modifier) {
+    val color = MaterialTheme.colors.primary
+    val typography = MaterialTheme.typography
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = "Local time:",
+            modifier = Modifier.align(CenterHorizontally),
+            style = typography.h3.copy(fontWeight = FontWeight.Light),
+            color = color,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "20:15:45",
+            modifier = Modifier.align(CenterHorizontally),
+            style = typography.h1.copy(fontWeight = FontWeight.SemiBold),
+            color = color
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RecomposeTheme {
+        Surface {
+            DigitalWatch()
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultDarkPreview() {
+    RecomposeTheme {
+        Surface {
+            DigitalWatch()
+        }
+    }
+}
