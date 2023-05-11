7b73bc410e96741746479f87eba50db5a013205a
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-05-11 11:33:12 +0100

    Refactor Computation.signalObserver

--- recompose/src/main/java/com/github/whyrising/recompose/subs/Computation.kt
+++ recompose/src/main/java/com/github/whyrising/recompose/subs/Computation.kt
@@ -5,7 +5,6 @@ import com.github.whyrising.recompose.subs.Computation.Companion.Ids.computation
 import com.github.whyrising.recompose.subs.Computation.Companion.Ids.signals_value
 import com.github.whyrising.y.core.collections.IPersistentMap
 import com.github.whyrising.y.core.collections.IPersistentVector
-import com.github.whyrising.y.core.collections.PersistentVector
 import com.github.whyrising.y.core.get
 import com.github.whyrising.y.core.m
 import com.github.whyrising.y.core.v
@@ -13,6 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -23,7 +23,6 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.transform
-import kotlinx.coroutines.flow.transformWhile
 import kotlin.coroutines.CoroutineContext
 
 typealias State = IPersistentMap<Ids, *>
@@ -44,51 +43,24 @@ class Computation(
 
   private fun compVal(it: Any?) = get<Any?>(it, computation_value)
 
-/*    override val signalObserver: Job
-
-    private fun deref(inputSignals: Signals) =
-      inputSignals.fold(v<Any?>()) { acc, reaction -> acc.conj(reaction.deref()) }
-
-    init {
-      signalObserver = reactionScope.launch(context = context) {
-        inputSignals.forEachIndexed { i, signal ->
-          reactionScope.launch(context) {
-            signal.collect { newSignal ->
-              val oldState = _state.value as State
-              val signals = deref(inputSignals).assoc(i, newSignal)
-              while (true) {
-                val newState = m(
-                  signals_value to signals, // cache
-                  computation_value to f(signals, compVal(oldState))
-                )
-                if (_state.compareAndSet(oldState, newState)) break
-              }
-            }
-          }
-        }
-      }
-    }*/
-
-  override val signalObserver: Job = combine(inputSignals) { it }
+  override val signalObserver: Job = combineV(inputSignals) { it }
     .distinctUntilChanged()
-    .map { Pair(it, _state.value as State) }
-    .transformWhile { pair ->
-      emit(pair)
-      pair.first != pair.second[signals_value]
-    }
-    .map { (signals, oldState) ->
-      // FIXME: write new combine function to work with PersistentVectors.
-      signals.fold(v<Any?>()) { acc, signal -> acc.conj(signal) }.conj(oldState)
-    }
-    .transform<PersistentVector<Any?>, State> { vec ->
-      val oldState = vec.peek() as State
-      val signals = vec.pop()
+    .transform<IPersistentVector<Any?>, State> { newSignals ->
       while (true) {
+        val oldState = _state.value as State
+        val oldSignals = oldState[signals_value] as Signals?
+        if (oldSignals != null && newSignals.fold(true) { acc, signal ->
+          if (!oldSignals.contains(signal)) return@fold false else acc
+        }
+        ) {
+          return@transform // skip
+        }
+
         val newState = m(
-          signals_value to signals, // cache
-          computation_value to f(signals, compVal(oldState))
+          signals_value to newSignals, // cache
+          computation_value to f(newSignals, compVal(oldState))
         )
-        if (_state.compareAndSet(oldState, newState)) break
+        if (_state.compareAndSet(oldState, newState)) return@transform
       }
     }
     .catch { th: Throwable -> throw RuntimeException(super.toString(), th) }
@@ -96,7 +68,6 @@ class Computation(
 
   override suspend fun collect(collector: FlowCollector<Any?>) =
     _state.collect { collector.emit((it as State)[computation_value]) }
-//    _state.collect { collector.emit(it) }
 
   override val state: StateFlow<Any?> by lazy {
     _state
@@ -113,5 +84,15 @@ class Computation(
   companion object {
     @Suppress("EnumEntryName")
     enum class Ids { signals_value, computation_value }
+
+    /**
+     * Same as [combine] but with [IPersistentVector] instead of [Array].
+     */
+    inline fun <reified T, R> combineV(
+      flows: Iterable<Flow<T>>,
+      crossinline transform: suspend (IPersistentVector<T>) -> R
+    ): Flow<R> = combine(flows) { ts: Array<T> ->
+      transform(ts.fold(v()) { acc, signal -> acc.conj(signal) })
+    }
   }
 }
