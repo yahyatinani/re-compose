87bb6d514ecd728cae3cfe9aec937d259aee89e4
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-08-25 14:08:43 +0100

    Update to Kotlin 1.9.0

--- buildSrc/src/main/kotlin/io/github/yahyatinani/recompose/Versions.kt
+++ buildSrc/src/main/kotlin/io/github/yahyatinani/recompose/Versions.kt
@@ -1,7 +1,7 @@
 package io.github.yahyatinani.recompose
 
 object Versions {
-  const val COMPOSE_COMPILER = "1.4.7"
+  const val COMPOSE_COMPILER = "1.5.1"
   const val KOTLIN = "1.7"
   const val JVM = "11"
 }
